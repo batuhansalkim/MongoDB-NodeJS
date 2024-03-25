556fba8b01e2b50ad076e2f3ca58482c3aca2dc1
batuhan <batusalkim11@gmail.com>
2024-03-26 01:45:21 +0300

    update

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const os = require("os");
 const path = require("path");
+const {add,substract} = require("./math");
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+console.log(add(5,3));
+console.log(substract(8,2))
 
-
-console.log(__dirname);
-console.log(__filename);
-
-console.log("*************")
-
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log(math.divide(5,2));
+// console.log(math.multiply(5,2));
+// console.log(math.substract(5,2));
 
 
-console.log(path.parse(__filename));
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log("*************")
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+
+// console.log(path.parse(__filename));
