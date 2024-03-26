01b2d789333d09d84d47952ec48c3e420102faba
batuhan <batusalkim11@gmail.com>
2024-03-26 03:38:37 +0300

    UPDATE

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
 const os = require("os");
 const path = require("path");
-const {add,substract} = require("./math");
+const { add, substract } = require("./math");
 
-console.log(add(5,3));
-console.log(substract(8,2))
+const fsPromises = require("fs").promises;
+
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, "files", "starter.txt"), "utf8");
+        console.log(data);
+
+        
+        await fsPromises.writeFile(path.join(__dirname, "files", "promiseWrite.txt"), data);
+
+        await fsPromises.appendFile(path.join(__dirname, "files", "promiseWrite.txt"), "\n\n\nNice to meet you");
+
+
+        await fsPromises.rename(path.join(__dirname, "files", "promiseWrite.txt"), path.join(__dirname, "files", "promiseCompleteWrite.txt"));
+
+        const newData = await fsPromises.readFile(path.join(__dirname, "files", "promiseCompleteWrite.txt"), "utf8");
+        console.log(newData);
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+fileOps();
+
+
+// const web = path.join(__dirname, "files", "starter.txt");
+// console.log(web);
+
+
+// fs.readFile(path.join(__dirname, "files", "starter.txt"), "utf-8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+// console.log("*********************");
+
+// fs.writeFile(path.join(__dirname, "files", "reply.txt"),"Nice to meet you batuhan", (err) => {
+//     if (err) throw err;
+//     console.log("Write Complete");
+// })
+
+// fs.writeFile(path.join(__dirname, "files", "batu.txt"), "batuhan deneme dosyasyı oluşturdu adi batu.txt", (err) => {
+//     if (err) throw err;
+//     console.log("dosya başarıyla oluşturuldu");
+
+//     fs.appendFile(path.join(__dirname, "files", "reply.txt"), "\n\n yes it is bro", (err) => {
+//         if (err) throw err;
+//         console.log("append complete");
+//     })
+// })
+
+// process.on("uncaughtException", err => {
+//     console.error(`There was an uncaught error: ${err}`);
+//     process.exit(1);
+// })
+
+
+
+// console.log(add(5,3));
+// console.log(substract(8,2))
 
 // console.log(math.divide(5,2));
 // console.log(math.multiply(5,2));
