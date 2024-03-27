1830c329b55c2dba2a245ef17bece6903b7bc233
batuhan <batusalkim11@gmail.com>
2024-03-28 02:52:37 +0300

    html sayfası ile calisma

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require("http");
-
+const fs = require("fs");
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method);
+
+    res.setHeader("Content-Type","text/html");
+    res.write("<head><title>Node Js Dersleri</title></head>")
+    res.write("<h1>Merhabalar</h1>");
+    res.end();
 })
 
 server.listen(3000,"localhost",()=>{
