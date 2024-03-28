1cfe4156c63341a1fa93867a58a07eccfafff859
batuhan <batusalkim11@gmail.com>
2024-03-28 03:30:45 +0300

    sayfa yönlendirme yapildi

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
 
-    res.setHeader("Content-Type","text/html");
-    res.write("<head><title>Node Js Dersleri</title></head>")
-    res.write("<h1>Merhabalar</h1>");
-    res.end();
+    res.setHeader("Content-type","text/html");
+
+    let path = "./views/";
+    switch(req.url){
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        default:
+            path += "404.html";
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+
+            res.end(data);
+        }
+    });
 })
 
-server.listen(3000,"localhost",()=>{
+server.listen(3000, "localhost", () => {
     console.log("3000 portu dinleniyor...");
     console.log("batuhan salkım");
 }) 
