714ae968d9e48d877b6459f90bcda304f458e9ef
batuhan <batusalkim11@gmail.com>
2024-03-28 07:22:50 +0300

    html içine nodejs ile veri yazma

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-    res.sendFile("./views/index.html", { root: __dirname });
+    res.render("index",{
+        title:"anasayfa"
+    });
 })
 
 
-app.get("/about",(req,res)=>{
-    res.sendFile("./views/about.html",{root:__dirname});
+app.get("/about", (req, res) => {
+    res.render("about",{
+        title:"Hakkımızda"
+    });
 })
 
-app.get("/about-us",(req,res)=>{
-    res.redirect("/about");
+app.get("/about-us", (req, res) => {
+    res.render("/about");
 })
 
-app.use((req,res)=>{
-    res.status(400).sendFile("./views/404.html",{root:__dirname});
+app.use((req, res) => {
+    res.status(400).render("404",{
+        title:"Sayfa Bulunamadı KNK"
+    });
 })
