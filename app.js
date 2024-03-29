31bc69b5e6db75f5fb1580655b5ab974642a0c66
batuhan <batusalkim11@gmail.com>
2024-03-29 03:48:57 +0300

    mongoDB Connection

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+const Blog = require("./models/blogs")
 
 
 const app = express();
 const dbURL = "mongodb+srv://Batu:batu123@nodeblog.scvno6o.mongodb.net/?retryWrites=true&w=majority&appName=NodeBlog"
-
-mongoose.connect(dbURL);
+mongoose.connect(dbURL)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
-
-app.listen(3000);
-
 app.use(express.static("public"));
 
 app.use(morgan("dev"));
+
+app.get("/add",(req,res)=>{
+    const blog = new Blog({
+        title:"yeni yazi 2",
+        short:"kisa aciklama",
+        long:"uzun aciklama",
+    })
+
+    blog.save()
+    .then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
 
 app.get("/", (req, res) => {
     res.render("index", {
